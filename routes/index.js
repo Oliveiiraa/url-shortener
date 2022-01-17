c07b2ea851d1a1415fc2a42a8d75914110b65c3b
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-01-17 12:54:53 -0300

    feat: add view stats

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const Link = require('../models/link');
+
+router.get('/:code/stats', async (req, res, next) => {
+  const code = req.params.code;
+
+  const resultado = await Link.findOne({ where: { code } });
+
+  if (!resultado) return res.sendStatus(404);
+
+  res.render('stats', { title: 'Encurtador', resultado: resultado });
+
+})
+
+router.get('/:code', async (req, res, next) => {
+  const code = req.params.code;
+
+  const resultado = await Link.findOne({ where: { code } });
+
+  if (!resultado) return res.sendStatus(404);
+
+  resultado.hits++;
+  await resultado.save();
+
+  res.redirect(resultado.url);
+})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,11 +40,17 @@ function generateCode() {
   }
   return text;
 }
-router.post('/new', (req, res, next) => {
+
+router.post('/new', async (req, res, next) => {
   const url = req.body.url;
   const code = generateCode();
 
-  res.send('http://localhost:3000/' + code);
+  const resultado = await Link.create({
+    url,
+    code
+  })
+
+  res.render('stats', { title: 'Encurtador', resultado: resultado.dataValues });
 })
 
 module.exports = router;
